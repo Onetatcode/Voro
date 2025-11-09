9980b865082b144a29bf6fbbbfa31b80eb23f735
Onetatcode <alikhanclues.@gmail.com>
2025-11-09 21:23:01 +0500

    Home with linkss done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Video from '../components/home/video.jsx'
+import HomeBottomText from '../components/home/HomeBottomText.jsx'
+import HomeHeroText from '../components/home/homeHeroText.jsx'
+
 
 function Home() {
   return (
-    <div className='h-screen w-screen bg-red-900'>
+    <div>
+    <div className='h-screen w-screen fixed'>
       <Video />
+    </div>
+    <div className='h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between'>
+      <HomeHeroText />
+      <HomeBottomText />
+    </div>
     </div>
   )
 }
